d590959a359350d773e225452bee3f5556954623
om061123 <r1047805@student.thomasmore.be>
2025-06-01 23:00:43 +0200

    eerste deel klaar

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-       
-    //    hier gaan we alle appointments opslaan in de localStorage 
-       let appointments = JSON.parse(localStorage.getItem('appointments')) || [];
+//    hier gaan we alle appointments opslaan in de localStorage
+let appointments = JSON.parse(localStorage.getItem("appointments")) || [];
 
-        document.getElementById('appointmentForm').addEventListener('submit', function(e) {
-            e.preventDefault();
-            
-            const title = document.getElementById('title').value;
-            const datetime = document.getElementById('datetime').value;
-                   });
+document
+  .getElementById("appointmentForm")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const title = document.getElementById("title").value;
+    const datetime = document.getElementById("datetime").value;
+
+    appointments.push({
+      id: Date.now(),
+      title,
+      datetime,
+      completed: false,
+    });
+    saveAppointments();
+    this.reset();
+
+  });
